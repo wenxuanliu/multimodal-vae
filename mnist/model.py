df2b0a966c8b8911a88934c5a90c506f4c4e413b
Mike Wu <me@mikewuis.me>
2017-10-17 00:22:45 -0700

    Added image only vae to compare

--- mnist/model.py
+++ mnist/model.py
@@ -119,3 +119,38 @@ class ProductOfExperts(nn.Module):
         pd_var = 1 / torch.sum(1 / var, dim=0)
         pd_logvar = torch.log(pd_var)
         return pd_mu, pd_logvar
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def encode(self, x):
+        h1 = self.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+          std = logvar.mul(0.5).exp_()
+          eps = Variable(std.data.new(std.size()).normal_())
+          return eps.mul(std).add_(mu)
+        else:
+          return mu
+
+    def decode(self, z):
+        h3 = self.relu(self.fc3(z))
+        return self.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
---
+++ mnist/train_imageonly.py
@@ -0,0 +1,160 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import sys
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import transforms
+
+from model import VAE
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_checkpoint(state, is_best, folder='./', filename='checkpoint.pth.tar'):
+    torch.save(state, os.path.join(folder, filename))
+    if is_best:
+        shutil.copyfile(os.path.join(folder, filename),
+                        os.path.join(folder, 'model_best.pth.tar'))
+
+
+def load_checkpoint(file_path, use_cuda=False):
+    """Return EmbedNet instance"""
+    if use_cuda:
+        checkpoint = torch.load(file_path)
+    else:
+        checkpoint = torch.load(file_path,
+                                map_location=lambda storage, location: storage)
+
+    vae = VAE()
+    vae.load_state_dict(checkpoint['state_dict'])
+    
+    return vae
+
+
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # Normalise by same number of elements as in reconstruction
+    KLD /= args.batch_size * 784
+
+    return BCE + KLD
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--log_interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    args = parser.parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=True, download=True,
+                       transform=transforms.ToTensor()),
+        batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=False, download=True, 
+                       transform=transforms.ToTensor()),
+        batch_size=args.batch_size, shuffle=True)
+
+    vae = VAE()
+    if args.cuda:
+        vae.cuda()
+
+    optimizer = optim.Adam(vae.parameters(), lr=args.lr)
+
+
+    def train(epoch):
+        model.train()
+        loss_meter = AverageMeter()
+
+        for batch_idx, (data, _) in enumerate(train_loader):
+            data = Variable(data)
+            if args.cuda:
+                data = data.cuda()
+            optimizer.zero_grad()
+            recon_batch, mu, logvar = model(data)
+            loss = loss_function(recon_batch, data, mu, logvar)
+            loss.backward()
+            loss_meter.update(loss.data[0], len(data))
+            optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss_meter.avg))
+
+        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, loss_meter.avg))
+
+
+    def test(epoch):
+        vae.eval()
+        test_loss = 0
+        for i, (data, _) in enumerate(test_loader):
+            if args.cuda:
+                data = data.cuda()
+            data = Variable(data, volatile=True)
+            recon_batch, mu, logvar = vae(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).data[0]
+
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+        return test_loss
+
+
+    best_loss = sys.maxint
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        loss = test()
+
+        is_best = loss < best_loss
+        best_loss = min(loss, best_loss)
+
+        save_checkpoint({
+            'state_dict': vae.state_dict(),
+            'best_loss': best_loss,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best, folder='./trained_models')
+
+        if is_best:
+            sample = Variable(torch.randn(64, 20))
+            if args.cuda:
+               sample = sample.cuda()
+
+            sample = vae.decode(sample).cpu()
+            save_image(sample.data.view(64, 1, 28, 28),
+                       './results/sample_image.png')
