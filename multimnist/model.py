006979537a1892f71c38d0b5deedffa6116a1cab
Mike Wu <me@mikewuis.me>
2017-10-21 23:45:50 -0700

    Added scripts to train imageonly and textonly in multimnist case

--- multimnist/model.py
+++ multimnist/model.py
@@ -18,6 +18,127 @@ from utils import n_characters
 from utils import SOS, EOS
 
 
+class MultimodalVAE(nn.Module):
+    def __init__(self, n_latents=20):
+        super(MultimodalVAE, self).__init__()
+        self.image_encoder = ImageEncoder(n_latents)
+        self.image_decoder = ImageDecoder(n_latents)
+        self.text_encoder = TextEncoder(n_latents)
+        self.text_decoder = TextDecoder(n_latents)
+        self.experts = ProductOfExperts()
+
+    def reparametrize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:  # return mean during inference
+            return mu
+
+    def encode_image(self, x):
+        return self.image_encoder(x)
+
+    def decode_image(self, x):
+        return self.image_decoder(x)
+
+    def encode_text(self, x):
+        return self.text_encoder(x)
+
+    def decode_text(self, x):
+        return self.text_decoder(x)
+
+    def forward(self, image=None, text=None):
+        # can't just put nothing
+        assert image is not None or text is not None
+        
+        if image is not None and text is not None:
+            # compute separate gaussians per modality
+            image_mu, image_logvar = self.encode_image(image)
+            text_mu, text_logvar = self.encode_text(text)
+            mu = torch.stack((image_mu, text_mu), dim=0)
+            logvar = torch.stack((image_logvar, text_logvar), dim=0)
+            # grab learned mixture weights and sample
+            mu, logvar = self.experts(mu, logvar)
+        elif image is not None:
+            mu, logvar = self.encode_image(image)
+        elif text is not None:
+            mu, logvar = self.encode_text(text)
+        
+        # reparametrization trick to sample
+        z = self.reparametrize(mu, logvar)
+        # reconstruct inputs based on that gaussian
+        image_recon = self.decode_image(z)
+        text_recon = self.decode_text(z)
+
+        return image_recon, text_recon, mu, logvar
+
+    def gen_latents(self, image, text):
+        # compute separate gaussians per modality
+        image_mu, image_logvar = self.encode_image(image)
+        text_mu, text_logvar = self.encode_text(text)
+        mu = torch.stack((image_mu, text_mu), dim=0)
+        logvar = torch.stack((image_logvar, text_logvar), dim=0)
+        
+        # grab learned mixture weights and sample
+        mu, logvar = self.experts(mu, logvar)
+        z = self.reparametrize(mu, logvar)
+        return z
+
+
+class ImageVAE(nn.Module):
+    def __init__(self, n_latents=20):
+        super(ImageVAE, self).__init__()
+        self.encoder = ImageEncoder(n_latents)
+        self.decoder = ImageDecoder(n_latents)
+        self.n_latents = n_latents
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def reparametrize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:  # return mean during inference
+            return mu
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+class TextVAE(nn.Module):
+    def __init__(self, n_latents=20):
+        super(TextVAE, self).__init__()
+        self.encoder = TextEncoder(n_latents)
+        self.decoder = TextDecoder(n_latents)
+        self.n_latents = n_latents
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def reparametrize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:  # return mean during inference
+            return mu
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
 class ImageEncoder(nn.Module):
     """This task is quite a bit harder than MNIST so we probably need 
     to use an RNN of some form. This will be good to get us ready for
