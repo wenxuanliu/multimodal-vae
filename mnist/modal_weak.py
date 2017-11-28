Bugfix in sign

mnist/modal_weak.py
@@ -48,7 +48,7 @@ def train_pipeline(out_dir, weak_perc_m1, weak_perc_m2, n_latents=20, batch_size
         batch_size=batch_size, shuffle=True)
 
     # load multimodal VAE
-    vae = MultimodalVAE(n_latents=n_latents, use_cuda=cuda)
+    vae = MultimodalVAE(n_latents=n_latents)
     if cuda:
         vae.cuda()
 
@@ -81,7 +81,6 @@ def train_pipeline(out_dir, weak_perc_m1, weak_perc_m2, n_latents=20, batch_size
             # depending on this flip, we decide whether or not to show a modality 
             # versus another one.
             flip = np.random.random()
-
             if flip < weak_perc_m1:
                 recon_image_2, recon_text_2, mu_2, logvar_2 = vae(image=image)
                 loss_2 = loss_function(mu_2, logvar_2, recon_image=recon_image_2, image=image, 
@@ -90,7 +89,7 @@ def train_pipeline(out_dir, weak_perc_m1, weak_perc_m2, n_latents=20, batch_size
                 loss += loss_2
 
             flip = np.random.random()
-            if flip > weak_perc_m2:
+            if flip < weak_perc_m2:
                 recon_image_3, recon_text_3, mu_3, logvar_3 = vae(text=text)
                 loss_3 = loss_function(mu_3, logvar_3, recon_image=recon_image_3, image=image, 
                                        recon_text=recon_text_3, text=text, lambda_xy=0., lambda_yx=1.)

mnist/train.py
@@ -61,6 +61,7 @@ def load_checkpoint(file_path, use_cuda=False):
 def loss_function(mu, logvar, recon_image=None, image=None, recon_text=None, text=None,
                   lambda_xy=1., lambda_yx=1.):
     image_BCE, text_BCE = 0, 0
+    batch_size = mu.size(0)
 
     if recon_image is not None and image is not None:
         image_BCE = lambda_xy * F.binary_cross_entropy(recon_image, image.view(-1, 784))
@@ -73,7 +74,7 @@ def loss_function(mu, logvar, recon_image=None, image=None, recon_text=None, tex
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    KLD /= args.batch_size * (784 / 3)  # for each pixel
+    KLD /= batch_size * (784 / 3)  # for each pixel
     return image_BCE + text_BCE + KLD
 
 

multimnist/modal_weak.py
@@ -105,7 +105,7 @@ def train_pipeline(out_dir, weak_perc_m1, weak_perc_m2, n_latents=20, batch_size
                 loss += loss_2
 
             flip = np.random.random()
-            if flip > weak_perc_m2:
+            if flip < weak_perc_m2:
                 recon_image_3, recon_text_3, mu_3, logvar_3 = vae(text=text)
                 loss_3 = loss_function(mu_3, logvar_3, recon_image=recon_image_3, image=image, 
                                        recon_text=recon_text_3, text=text, kl_lambda=kl_lambda, 